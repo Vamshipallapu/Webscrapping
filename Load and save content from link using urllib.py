60ee6f4011734e0b1aac9e8b75a2a1ab0274e2f1
VAMSHI PALLAPU <44836993+Vamshipallapu@users.noreply.github.com>
2023-10-09 16:55:40 +0530

    Delete Load and save content from link using urllib.py

--- Load and save content from link using urllib.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-urllib.request.urlretrieve('https://www.samsclub.com/robots.txt') #urllib.request returns a tuple with the filename and HTTP headers
-#('C:\\Users\\*****\AppData\\Local\\Temp\\tmpjs_cktnc', <http.client.HTTPMessage object at 0x04029110>)
-
-urllib.request.urlretrieve(link,"testrobots.txt") #urlretrieve(url, filename=None)
-#('testrobots.txt', <http.client.HTTPMessage object at 0x04322DF0>)
-
